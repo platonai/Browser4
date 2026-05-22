6ebf3056298b0d21401982babb835840dc4ad2fc
platon.AI <galaxyeye@live.cn>
2026-05-22 15:16:24 +0800

    refactor: simplify shutdown hook thread implementation in ChromeLauncher

--- browser4-core/browser4-browser/src/main/kotlin/ai/platon/browser4/driver/chrome/ChromeLauncher.kt
+++ browser4-core/browser4-browser/src/main/kotlin/ai/platon/browser4/driver/chrome/ChromeLauncher.kt
@@ -117,13 +117,7 @@ class ChromeLauncher constructor(
 
     private val isClosed get() = closed.get()
     private val isActive get() = AppContext.isActive && !Thread.currentThread().isInterrupted
-    private val shutdownHookThread = Thread {
-        // the upper layer should also close the launcher
-        if (!isClosed) {
-            // sleepSeconds(10)
-            this.close()
-        }
-    }
+    private val shutdownHookThread = Thread { this.close() }
 
     private fun clearProcessMarkers() {
         BrowserFilesPatch.clearProcessMarkers(userDataDir)
