829f16d5bd1454e13866cc03e1ce0837ff48adca
platon.AI <galaxyeye@live.cn>
2026-05-28 09:28:38 +0800

    feat: add PulsarBeansAutoConfiguration for enhanced configuration management

--- browser4-rest/src/main/kotlin/ai/platon/pulsar/rest/ApiApplication.kt
+++ browser4-rest/src/main/kotlin/ai/platon/pulsar/rest/ApiApplication.kt
@@ -10,7 +10,6 @@ import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.ImportResource
 
 @SpringBootApplication
-@ImportResource("classpath:rest-beans/app-context.xml")
 @ComponentScan(
     "ai.platon.pulsar.boot.autoconfigure",
     "ai.platon.pulsar.rest"
