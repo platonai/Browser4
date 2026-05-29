86afcd896fde62e6ad9ca19af4b91f4cb9422418
platon.AI <galaxyeye@live.cn>
2026-05-29 16:30:26 +0800

    refactor: copy all code from pulsar-protocol to browser4-protocol, we will refactor the code step by step

---
+++ browser4-core/browser4-plugins/browser4-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/impl/DefaultBrowserFactory.kt
@@ -0,0 +1,34 @@
+package ai.platon.pulsar.protocol.browser.impl
+
+import ai.platon.pulsar.browser.Browser
+import ai.platon.pulsar.browser.BrowserId
+import ai.platon.pulsar.browser.BrowserLauncher
+import ai.platon.pulsar.browser.common.BrowserSettings
+import ai.platon.pulsar.chrome.util.ChromeOptions
+import ai.platon.pulsar.chrome.util.LauncherOptions
+import ai.platon.pulsar.common.browser.BrowserType
+import ai.platon.pulsar.common.config.ImmutableConfig
+
+class DefaultBrowserFactory(
+    conf: ImmutableConfig = ImmutableConfig(loadDefaults = true),
+    settings: BrowserSettings = BrowserSettings(conf)
+) : AbstractBrowserFactory(conf, settings) {
+    private val launchers = mapOf(
+        BrowserType.PULSAR_CHROME to PulsarBrowserLauncher()
+    )
+
+    constructor(conf: ImmutableConfig) : this(conf, BrowserSettings(conf))
+
+    @Synchronized
+    override fun launch(
+        browserId: BrowserId, launcherOptions: LauncherOptions, launchOptions: ChromeOptions
+    ): Browser = getLauncher(browserId.browserType).launch(browserId, launcherOptions, launchOptions)
+
+    @Synchronized
+    override fun connect(browserType: BrowserType, port: Int, settings: BrowserSettings): Browser =
+        getLauncher(browserType).connect(port, settings)
+
+    private fun getLauncher(browserType: BrowserType): BrowserLauncher {
+        return launchers[browserType] ?: throw IllegalArgumentException("Unknown browser type: $browserType")
+    }
+}
