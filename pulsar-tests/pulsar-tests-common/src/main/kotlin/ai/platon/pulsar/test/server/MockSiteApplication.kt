82c135bc4956bacad28e281f8b05ff0aafcf14de
platon.AI <galaxyeye@live.cn>
2026-05-31 22:07:57 +0800

    chore: remove unused imports and add BrowserFactory to WebDriverService

--- pulsar-tests/pulsar-tests-common/src/main/kotlin/ai/platon/pulsar/test/server/MockSiteApplication.kt
+++ pulsar-tests/pulsar-tests-common/src/main/kotlin/ai/platon/pulsar/test/server/MockSiteApplication.kt
@@ -1,12 +1,9 @@
 package ai.platon.pulsar.test.server
 
-import ai.platon.pulsar.boot.autoconfigure.PulsarAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(PulsarAutoConfiguration::class)
 class MockSiteApplication
 
 fun main() {
