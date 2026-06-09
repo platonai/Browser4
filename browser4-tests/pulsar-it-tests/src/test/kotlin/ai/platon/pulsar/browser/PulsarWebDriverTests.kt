37c68899dc588fc100e2e55d382ffc6282e21a3c
platon.AI <galaxyeye@live.cn>
2026-06-09 10:53:07 +0800

    fix(tests): correct method naming conventions in PulsarWebDriverTests

--- browser4-tests/pulsar-it-tests/src/test/kotlin/ai/platon/pulsar/browser/PulsarWebDriverTests.kt
+++ browser4-tests/pulsar-it-tests/src/test/kotlin/ai/platon/pulsar/browser/PulsarWebDriverTests.kt
@@ -87,7 +87,7 @@ class PulsarWebDriverTests : WebDriverTestBase() {
 
     @Test
     @DisplayName("test selectFirstPropertyValueOrNull")
-    fun testSelectfirstpropertyvalueornull() =
+    fun testSelectFirstPropertyValueOrNull() =
         runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
             val selector = "input[id=input]"
 
@@ -100,7 +100,7 @@ class PulsarWebDriverTests : WebDriverTestBase() {
 
     @Test
     @DisplayName("test selectPropertyValueAll")
-    fun testSelectpropertyvalueall() = runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
+    fun testSelectPropertyValueAll() = runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
         val selector = "input"
 
         val propValues = driver.selectPropertyValueAll(selector, "tagName")
@@ -110,7 +110,7 @@ class PulsarWebDriverTests : WebDriverTestBase() {
 
     @Test
     @DisplayName("test setProperty")
-    fun testSetproperty() = runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
+    fun testSetProperty() = runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
         val selector = "input"
         val propName = "value"
 
@@ -122,7 +122,7 @@ class PulsarWebDriverTests : WebDriverTestBase() {
 
     @Test
     @DisplayName("test setPropertyAll")
-    fun testSetpropertyall() = runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
+    fun testSetPropertyall() = runEnhancedWebDriverTest(simpleDomURL, browser) { driver ->
         val selector = "input"
         val propName = "value"
 
@@ -135,7 +135,7 @@ class PulsarWebDriverTests : WebDriverTestBase() {
 
     @Test
     @DisplayName("test deleteCookies")
-    fun testDeletecookies() = runEnhancedWebDriverTest("$assetsPBaseURL/cookie.html", browser) { driver ->
+    fun testDeleteCookies() = runEnhancedWebDriverTest("$assetsPBaseURL/cookie.html", browser) { driver ->
         var cookies = driver.getCookies()
 
         printlnPro(cookies.toString())
