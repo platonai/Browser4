81d98e1ccacc38336fcd5e2c0c1bb8c6a12a1de6
platon.AI <galaxyeye@live.cn>
2026-06-28 21:44:50 +0800

    chore

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,7 @@
 
 . "$(dirname "$0")/tools/install-powershell.sh"
 
+echo "It is strongly recommended to launch `pwsh` and run the .ps1commands directly within the `pwsh` terminal."
+
 # Call bin/build/build.ps1 with all passed arguments
 pwsh bin/build/build.ps1 "$@"
