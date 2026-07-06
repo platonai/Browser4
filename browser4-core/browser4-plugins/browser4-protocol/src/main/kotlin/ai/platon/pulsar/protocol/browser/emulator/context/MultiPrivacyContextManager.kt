10f2fdb1c51dc9829b7c6c3e7260077e199143b2
platon.AI <galaxyeye@live.cn>
2026-07-06 16:58:46 +0800

    refactor browser4-protocol

--- browser4-core/browser4-plugins/browser4-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/emulator/context/MultiPrivacyContextManager.kt
+++ browser4-core/browser4-plugins/browser4-protocol/src/main/kotlin/ai/platon/pulsar/protocol/browser/emulator/context/MultiPrivacyContextManager.kt
@@ -16,7 +16,6 @@
 package ai.platon.pulsar.protocol.browser.emulator.context
 
 import ai.platon.browser4.chrome.ChromeDestroyer
-import ai.platon.browser4.protocol.browser.DefaultWebDriverPoolManager
 import ai.platon.pulsar.browser.BrowserProfile
 import ai.platon.pulsar.browser.privacy.AbstractPrivacyContext
 import ai.platon.pulsar.browser.privacy.PrivacyContext
@@ -35,6 +34,7 @@ import ai.platon.pulsar.persist.AbstractWebPage
 import ai.platon.pulsar.persist.RetryScope
 import ai.platon.pulsar.persist.WebPage
 import ai.platon.pulsar.persist.model.GoraWebPage
+import ai.platon.pulsar.protocol.browser.DefaultWebDriverPoolManager
 import ai.platon.pulsar.protocol.browser.driver.WebDriverPoolManager
 import ai.platon.pulsar.skeleton.CoreMetrics
 import ai.platon.pulsar.skeleton.common.AppSystemInfo
