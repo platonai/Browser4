360ac3a08621c98758f40b43e836d45b98e39c91
platon.AI <galaxyeye@live.cn>
2026-07-14 06:56:42 +0800

    chore

--- cli/browser4-cli/query_test2.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    DOM_TEXT(DOM) AS full_text,
-    DOM_TAG_NAME(DOM) AS tag,
-    DOM_CLASS_NAME(DOM) AS classes
-FROM DOM_LOAD_AND_SELECT(@url, '#product-list > div')
-WHERE DOM_IS_NOT_NIL(DOM)
-LIMIT 2
--- cli/browser4-cli/query_v2.sql
+++
@@ -1,9 +0,0 @@
-SELECT
-    DOM_FIRST_TEXT(DOM, '[class*="product-title"]') AS title,
-    DOM_FIRST_TEXT(DOM, '[class*="product-price"]') AS price,
-    DOM_FIRST_HREF(DOM, 'a') AS link,
-    DOM_FIRST_IMG(DOM, 'img') AS image,
-    DOM_FIRST_ATTR(DOM, 'data-category-id') AS category_id,
-    DOM_FIRST_ATTR(DOM, 'id') AS product_id
-FROM DOM_LOAD_AND_SELECT(@url, '#product-list > div')
-WHERE DOM_IS_NOT_NIL(DOM)
